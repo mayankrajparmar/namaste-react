41099f9c8e142af6f09e941ed71b7a18e6258ed6
Mayank Raj Parmar <mayankrajparmar@gmail.com>
2025-01-22 21:01:04 +0530

    Assignment 1 completed

---
+++ assignment_01/index.js
@@ -0,0 +1,21 @@
+// Using JavaScript
+//     const heading = document.createElement("h1");
+//     heading.innerHTML = "Namaste World";
+//     const root = document.getElementById("root");
+//     root.appendChild(heading);
+
+// Using React
+const heading = React.createElement("h1", { id: "title" }, "Namaste World");
+
+const heading2 = React.createElement(
+  "h2",
+  { id: "title2" },
+  "To Kaise Hai Aap Log"
+);
+
+const container = React.createElement("div", { id: "container" }, [
+  heading,
+  heading2,
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
